78afde96ca7e3657f348d877a53529e9f00e6298
Alexander Thoukydides <thoukydides@users.noreply.github.com>
2026-01-18 09:00:19 +0000

    Normalise line endings

--- src/clean_issue.ts
+++ src/clean_issue.ts
@@ -5,6 +5,9 @@ import { Issue } from './get_issue.js';
 import * as core from '@actions/core';
 import { plural } from './utils.js';
 
+// Match line endings (allowing CRLF, CR, or LF)
+const LINE_ENDING = /\r\n|(?<!\r)\n|\r(?!\n)/;
+
 // Match ANSI colour codes (including textual representation of escape code)
 // eslint-disable-next-line no-control-regex
 const ANSI_ESCAPE = /(?:\x1B|ESC)\[[0-9;]*[msuK]/g;
@@ -18,11 +21,17 @@ export function cleanIssue(issue: Issue): Issue {
     const botCount = rawComments.length - humanComments.length;
     if (0 < botCount) core.info(`Excluded ${plural(botCount, 'comment')} by bots`);
 
-    // Remove anything resembling ANSI codes from the body and comments
-    const stripAnsiCodes = (text: string): string => text.replaceAll(ANSI_ESCAPE, '');
-    const body = stripAnsiCodes(rawBody);
-    const comments = humanComments.map(({ body, ...rest }) => ({ body: stripAnsiCodes(body), ...rest }));
+    // Normalise line endings and remove ANSI codes from the body and comments
+    const body = cleanBody(rawBody);
+    const comments = humanComments.map(({ body, ...rest }) => ({ body: cleanBody(body), ...rest }));
 
     // Return the cleaned result
     return { body, comments, ...restIssue };
+}
+
+// Clean an issue or comment body
+function cleanBody(text: string): string {
+    return text
+        .replaceAll(LINE_ENDING, '\n')
+        .replaceAll(ANSI_ESCAPE, '');
 }
