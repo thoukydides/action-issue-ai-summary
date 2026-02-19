b2c13924ef7e9474ce05ea78d4fa339ee1a94b66
Alexander Thoukydides <thoukydides@users.noreply.github.com>
2026-02-19 09:14:42 +0000

    Actually use the new code

--- src/truncate_text.ts
+++ src/truncate_text.ts
@@ -80,47 +80,6 @@ export function truncateText(text: string, maxChars: number): string {
     if (text.length <= maxChars)                return text;
     if (maxChars < TRUNCATION_MARKER.length)    return '';
 
-    // Partition the text with different granularity
-    const textPartitions = [
-        text.split(/(\n\n+)/),  // (paragraphs)
-        text.split(/(\n+)/),    // (lines)
-        [...sentenceSegmenter.segment(text)].map(({ segment }) => segment),
-        [...wordSegmenter    .segment(text)].map(({ segment }) => segment)
-    ];
-
-    // Search for a partition under the target length
-    const choosePrefix = (partitions: string[][], maxChars: number): string[] => {
-        const minChars = Math.floor(maxChars * 0.8);
-        let prefix: string[] = [];
-        for (const partition of partitions) {
-            // Find longest length of this partition under the limit
-            prefix = [];
-            let length = 0;
-            for (const segment of partition) {
-                if (maxChars < length + segment.length) break;
-                prefix.push(segment);
-                length += segment.length;
-            }
-            if (minChars <= length) break;
-        }
-        return prefix;
-    };
-    const chooseSuffix = (partitions: string[][], maxChars: number): string[] =>
-        choosePrefix(partitions.map(p => p.toReversed()), maxChars).toReversed();
-
-    // Cut out the middle of the text to end up under the target
-    const maxPrefixChars = Math.floor((maxChars - TRUNCATION_MARKER.length) / 2);
-    const prefix = choosePrefix(textPartitions, maxPrefixChars).join('').trimEnd();
-    const maxSuffixChars = Math.floor(maxChars - prefix.length - TRUNCATION_MARKER.length);
-    const suffix = chooseSuffix(textPartitions, maxSuffixChars).join('').trimStart();
-    return `${prefix}${TRUNCATION_MARKER}${suffix}`;
-}
-
-
-export function truncateText2(text: string, maxChars: number): string {
-    if (text.length <= maxChars)                return text;
-    if (maxChars < TRUNCATION_MARKER.length)    return '';
-
     // Get the lengths of segments in the text according to a segmenter or regex
     const getSegmentOffsets = (str: string, segmenter: Intl.Segmenter): number[] => {
         const lengths: number[] = [];
